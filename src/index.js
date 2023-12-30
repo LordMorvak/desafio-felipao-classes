cbb6b774b0578cb0289b48c53e91bfce9d777044
LordMorvak <lordmorvaky@gmail.com>
2023-12-30 18:43:52 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 // criando uma classe chamada hero
+class hero {
+    constructor(name, age, type) {
+        this.name = name
+        this.age = age
+        this.type = type
+    }
+    // criando um switch case para definir os tipos de persogens e tipos de ataques.
+    attack() {
+        let attacking
+
+        switch (this.type) {
+            case 'warrior':
+                attacking = 'sword'
+                break
+
+            case 'mage':
+                attacking = 'magic'
+                break
+
+            case 'monk':
+                attacking = 'martial arts'
+                break
+
+            case 'ninja':
+                attacking = 'shuriken'
+                break
+
+            default:
+                attacking = 'attacked using'
+        }
+
+        console.log(`The ${this.type} attacked using ${attacking}`)
+    }
+}
+
+let firstHero = new hero('LordMorvak', 44, 'warrior')
+let secondHero = new hero('LordMorvak', 44, 'mage')
+let thirdHero = new hero('LordMorvak', 44, 'monk')
+let fourthHero = new hero('LordMorvak', 44, 'ninja')
+
+firstHero.attack()
+secondHero.attack()
+thirdHero.attack()
+fourthHero.attack()
